e540d68916181820e9fbf4e43cef39bd66024798
Jung Jae Hoon <pow852014@gmail.com>
2018-10-15 14:13:08 +0900

    fnt c# application

---
+++ HelloWrld/HelloWrld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    class Emp { }
+    class Dept { }    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Hello C# ~" + args[0]);
+        }
+    }
+}
